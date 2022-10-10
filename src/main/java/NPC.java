03bd68304c0afb0d7387cff411513741748e871d
TOCHMY <tommyisolna@hotmail.com>
2022-10-10 15:20:17 +0200

    NPC class update with tests

--- src/main/java/NPC.java
+++ src/main/java/NPC.java
@@ -2,19 +2,20 @@ import java.util.Random;
 
 public class NPC {
 
-    private String name;
-    private boolean isInteractive; //Går att interagera med NPC eller är en del av världen
-    private boolean isEnemy;
+    //Set NPC movement range? <- så typ en specifik NPC kan inte röra sig längre än 2 pixlar
+    // från sin initialPosition.
+    private final String name;
     private int xPosition;
     private int yPosition;
     private int[] currentPosition = new int[2];
+    private final int[] initialPosition = new int[2];
 
-    public NPC(String name, boolean isInteractive, boolean isEnemy, int xPosition, int yPosition) {
+    public NPC(String name, int xPosition, int yPosition) {
         this.name = name;
-        this.isInteractive = isInteractive;
-        this.isEnemy = isEnemy;
         this.xPosition = xPosition;
         this.yPosition = yPosition;
+        initialPosition[0] = xPosition;
+        initialPosition[1] = yPosition;
         setCurrentPosition(xPosition, yPosition);
     }
 
@@ -27,36 +28,42 @@ public class NPC {
         return currentPosition;
     }
 
-    public int getxPosition() {
-        return xPosition;
-    }
-    public int getyPosition() {
-        return yPosition;
-    }
-
-    //Public for testing purpose
-    public int generateRandomMovementNumber() {
+    private int generateRandomMovementNumber() {
         Random rand = new Random();
-        return rand.nextInt(5) + 1;
+        return rand.nextInt(4) + 1;
     }
 
+    public void moveUp() {
+        yPosition++;
+        setCurrentPosition(xPosition, yPosition);
+    }
+    public void moveDown() {
+        yPosition--;
+        setCurrentPosition(xPosition, yPosition);
+    }
+    public void moveRight() {
+        xPosition++;
+        setCurrentPosition(xPosition, yPosition);
+    }
+    public void moveLeft() {
+        xPosition--;
+        setCurrentPosition(xPosition, yPosition);
+    }
     //When an NPC is idle, meaning not interacting with the Player in any way, the NPC has the ability to
     // walk around randomly on the map.
     public void idleMove() {
         int movementDirection = generateRandomMovementNumber();
-        //1 = right, 2 = left, 3 = up, 4 = down, 5 = no movement
+        //1 = right, 2 = left, 3 = up, 4 = down
         switch (movementDirection) {
             case 1 -> xPosition++;
             case 2 -> xPosition--;
             case 3 -> yPosition++;
             case 4 -> yPosition--;
-            case 5 -> {}
             default -> {
             }
         }
         setCurrentPosition(xPosition, yPosition);
     }
-
 
     @Override
     public String toString() {
@@ -64,7 +71,7 @@ public class NPC {
     }
 
     public static void main(String[] args) {
-        NPC randy = new NPC("Randy", true, false, 50 , 50);
+        NPC randy = new NPC("Randy", 50 , 50);
         System.out.println(randy);
         randy.idleMove();
         System.out.println(randy);
--- src/test/java/NPCtest.java
+++ src/test/java/NPCtest.java
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -5,18 +6,63 @@ import org.junit.jupiter.params.provider.ValueSource;
 import static org.junit.jupiter.api.Assertions.*;
 
 
-public class NPCtest {
+class NPCtest {
 
-    NPC randy = new NPC("Randy", true, false, 50 , 50);
-
-    @Test
-    void testMovementUp() {
-        assertEquals();
-    }
+    //Hur testa random movement? Kolla med Henke B
 
     @Test
     void testIdleMovement_makeSureTheNPCMoved() {
-        assertArrayEquals;
+        NPC randy = new NPC("Randy", 50, 50);
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{50, 50});
+        randy.idleMove();
+        assertNotEquals(randy.getCurrentPosition(), new int[]{50, 50});
     }
 
+    @Test
+    void testUpMovement() {
+        NPC randy = new NPC("Randy", 50, 50);
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{50, 50});
+        randy.moveUp();
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{50, 51});
+        randy.moveUp();
+        randy.moveUp();
+        randy.moveUp();
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{50, 54});
+    }
+    @Test
+    void testDownMovement() {
+        NPC randy = new NPC("Randy", 50, 50);
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{50, 50});
+        randy.moveDown();
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{50, 49});
+    }
+    @Test
+    void testLeftMovement() {
+        NPC randy = new NPC("Randy", 50, 50);
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{50,50});
+        randy.moveLeft();
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{49, 50});
+    }
+
+    @Test
+    void testRightMovement() {
+        NPC randy = new NPC("Randy", 50, 50);
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{50,50});
+        randy.moveRight();
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{51, 50});
+    }
+
+    @Test
+    void testMovement_moveInACircle() {
+        NPC randy = new NPC("Randy", 50, 50);
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{50, 50});
+        randy.moveUp();
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{50, 51});
+        randy.moveRight();
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{51, 51});
+        randy.moveDown();
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{51, 50});
+        randy.moveLeft();
+        assertArrayEquals(randy.getCurrentPosition(), new int[]{50, 50});
+    }
 }
