0d80191bcb9e587130c192e0202d2a665330ca00
Johan Johansson <johanjmusic@gmail.com>
2022-10-11 12:57:34 +0200

    Added instance variables cost and strength, tested creation of gemstone

--- src/main/java/GemStone.java
+++ src/main/java/GemStone.java
@@ -1,11 +1,23 @@
 public class GemStone {
-    private MagicColor color;
+    private final MagicColor color;
+    private final int cost;
+    private final int strength;
 
-    public GemStone(MagicColor color) {
+    public GemStone(MagicColor color, int strength, int cost) {
         this.color = color;
+        this.cost = cost;
+        this.strength = strength;
     }
 
     public MagicColor getColor() {
         return color;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getStrength() {
+        return strength;
     }
 }
--- src/test/java/GemStoneTest.java
+++ src/test/java/GemStoneTest.java
@@ -5,13 +5,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GemStoneTest {
 
     @Test
-    public void createGemstoneWithColor(){
-
-        GemStone gemStone = new GemStone(MagicColor.BLUE);
+    public void createGemstoneWithColorBoostAndCost(){
+        GemStone gemStone = new GemStone(MagicColor.BLUE,5,2);
         assertEquals(MagicColor.BLUE, gemStone.getColor());
-
-
+        assertEquals(5, gemStone.getStrength());
+        assertEquals(2, gemStone.getCost());
     }
+
+
+
 
 
 }
