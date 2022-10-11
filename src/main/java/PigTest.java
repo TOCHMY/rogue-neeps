e24871fa28eb4bdf70c1bdb07f98c7c57edb8fbb
Nicholas Miettinen <93824748+nicomie@users.noreply.github.com>
2022-10-11 16:18:27 +0200

    test movement

--- src/main/java/PigTest.java
+++ src/main/java/PigTest.java
@@ -5,14 +5,38 @@ import org.junit.Test;
 public class PigTest {
 
     @Test
-    public void TestPigKillable(){
-
+    public void TestgKillable(){
+        Pig pig = new Pig();
+        pig.die();
+        assertEquals(0, pig.getHealth());
     }
 
+    @Test
+    public void TestXp(){
+        Pig pig = new Pig();
+        assertNotEquals(0, pig.getXp());
+    }
+
+
+    @Test
+    public void TestStartPosition(){
+        Pig pig = new Pig();
+        assertEquals(0, pig.getCurrentX());
+        assertEquals(0, pig.getCurrentY());
+    }
+
+    @Test
+    public void TestCustomStartPos(){
+        Pig pig = new Pig(20, 20);
+        assertEquals(20, pig.getCurrentX());
+        assertEquals(20, pig.getCurrentY());
+    }
     @Test
     public void TestPigMovement(){
         Pig pig = new Pig();
         pig.moveUp();
         assertEquals(10, pig.getCurrentY());
     }
+
+
 }
