f34d8a822c80002588ad58ed28e58c2d1e3c08e4
Johan Johansson <johanjmusic@gmail.com>
2022-10-11 13:05:30 +0200

    Tested and added min and vax values of cost and strength in gemstones

--- src/main/java/GemStone.java
+++ src/main/java/GemStone.java
@@ -4,6 +4,11 @@ public class GemStone {
     private final int strength;
 
     public GemStone(MagicColor color, int strength, int cost) {
+        if(strength < 1 || strength > 10)
+            throw new IllegalArgumentException("Strength must be between 1-10");
+        if(cost < 1 || cost > 10)
+            throw new IllegalArgumentException("Cost must be between 1-10");
+
         this.color = color;
         this.cost = cost;
         this.strength = strength;
--- src/test/java/GemStoneTest.java
+++ src/test/java/GemStoneTest.java
@@ -1,15 +1,34 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class GemStoneTest {
 
     @Test
-    public void createGemstoneWithColorBoostAndCost(){
+    public void testCreateGemstoneWithColorStrengthAndCost(){
         GemStone gemStone = new GemStone(MagicColor.BLUE,5,2);
         assertEquals(MagicColor.BLUE, gemStone.getColor());
         assertEquals(5, gemStone.getStrength());
         assertEquals(2, gemStone.getCost());
+    }
+
+    @Test
+    public void testInvalidStrength(){
+        assertThrows(IllegalArgumentException.class, () ->
+                new GemStone(MagicColor.BLUE, 0, 10));
+        assertThrows(IllegalArgumentException.class, () ->
+                new GemStone(MagicColor.BLUE, 11, 10));
+    }
+
+    @Test
+    public void testInvalidCost(){
+        assertThrows(IllegalArgumentException.class, () ->
+                new GemStone(MagicColor.BLUE, 10, 0));
+
+        assertThrows(IllegalArgumentException.class, () ->
+                new GemStone(MagicColor.BLUE, 10, 11));
+
     }
 
 
