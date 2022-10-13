676919cc95fbdb12ebaca287fd49a4db45d77759
Johan Johansson <johanjmusic@gmail.com>
2022-10-13 22:33:21 +0200

    Moved test methods from WeaponTest to ItemTest

--- src/test/java/ArmorTest.java
+++ src/test/java/ArmorTest.java
@@ -7,24 +7,19 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ArmorTest {
 
-
     List<MagicSocket> THREE_SOCKETS;
 
     @BeforeEach
     public void createSockets(){
-
         THREE_SOCKETS = List.of
                 (new MagicSocket(MagicColor.RED),
                         new MagicSocket(MagicColor.RED),
                         new MagicSocket(MagicColor.PURPLE));
     }
 
-
-
     @Test
     public void testCreateArmor(){
         Armor armor = new Armor(50, THREE_SOCKETS);
-
         assertEquals(50, armor.getStrength());
         assertEquals(THREE_SOCKETS, armor.getSockets());
     }
