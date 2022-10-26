948c5df129e9d729f260173d52e8990d4e7466c6
Nicholas Miettinen <93824748+nicomie@users.noreply.github.com>
2022-10-26 16:00:21 +0200

    map

--- src/test/java/map/TruthTableForPlayerMovement.java
+++ src/test/java/map/TruthTableForPlayerMovement.java
@@ -15,6 +15,7 @@ public class TruthTableForPlayerMovement {
     Map map;
     Room roomA;
     Position tileToTest;
+    Position tunnelPosition;
 
     @BeforeEach
     public void initEach(){
@@ -23,7 +24,67 @@ public class TruthTableForPlayerMovement {
         human.setMap(map);
         roomA = new Room("A", 27,8, new Tile(2,7), map);
         map.addRoom(roomA);
+
+        Room roomC = new Room("C", 23,6, new Tile(new Position(14,26)), map);
+        map.addRoom(roomC);
+
+        Tile tunnelStart = map.getMap()[10][31];
+        Tile tunnelEnd = map.getMap()[13][31];
+        Tunnel tunnel1 = new Tunnel("A", "C",tunnelStart,tunnelEnd);
+        map.addTunnel(tunnel1);
+
+        tunnelPosition = tunnelStart.getPosition();
+
         tileToTest = new Position(3, 9);
+    }
+
+    @Test
+    public void Human_StrBelow10_FloorTile_Expect_True(){
+        assertTrue(human.canMove(map.getTile(tileToTest)));
+    }
+    @Test
+    public void Human_StrAbove10Below20_FloorTile_Expect_True(){
+        human.getStats().addStrength(10);
+        assertTrue(human.canMove(map.getTile(tileToTest)));
+    }
+    @Test
+    public void Human_StrAbove20_FloorTile_Expect_True(){
+        human.getStats().addStrength(21);
+        assertTrue(human.canMove(map.getTile(tileToTest)));
+    }
+
+    @Test
+    public void Human_StrBelow10_TunnelTile_Expect_True(){
+        assertTrue(human.canMove(map.getTile(tunnelPosition)));
+    }
+    @Test
+    public void Human_StrAbove10Below20_TunnelTile_Expect_True(){
+        human.getStats().addStrength(10);
+        assertTrue(human.canMove(map.getTile(tunnelPosition)));
+    }
+    @Test
+    public void Human_StrAbove20_TunnelTile_Expect_True(){
+        human.getStats().addStrength(21);
+        assertTrue(human.canMove(map.getTile(tunnelPosition)));
+    }
+
+    @Test
+    public void Human_StrBelow10_SwampTile_Expect_True(){
+        roomA.addSwampTile(tileToTest);
+        assertFalse(human.canMove(map.getTile(tileToTest)));
+    }
+
+    @Test
+    public void Human_StrAbove10Below20_SwampTile_Expect_True(){
+        roomA.addSwampTile(tileToTest);
+        human.getStats().addStrength(10);
+        assertFalse(human.canMove(map.getTile(tileToTest)));
+    }
+    @Test
+    public void Human_StrAbove20_SwampTile_Expect_True(){
+        roomA.addSwampTile(tileToTest);
+        human.getStats().addStrength(21);
+        assertTrue(human.canMove(map.getTile(tileToTest)));
     }
 
 
@@ -34,15 +95,15 @@ public class TruthTableForPlayerMovement {
     }
 
     @Test
-    public void Human_StrAbove10_Below_20_WaterTile_Expect_False(){
+    public void Human_StrAbove10Below20_WaterTile_Expect_True(){
         roomA.addWaterTile(tileToTest);
         human.getStats().addStrength(10);
-        assertFalse(human.canMove(map.getTile(tileToTest)));
+        assertTrue(human.canMove(map.getTile(tileToTest)));
     }
     @Test
-    public void Human_StrAbove20_WaterTile_Expect_False(){
+    public void Human_StrAbove20_WaterTile_Expect_True(){
         roomA.addWaterTile(tileToTest);
-        human.getStats().addStrength(10);
-        assertFalse(human.canMove(map.getTile(tileToTest)));
+        human.getStats().addStrength(21);
+        assertTrue(human.canMove(map.getTile(tileToTest)));
     }
 }
